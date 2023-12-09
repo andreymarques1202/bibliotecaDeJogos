acc2ad653fcd1be0c0f65c6ec154d98510d81ae0
Andrey Marques <andreymarques822@gmail.com>
2023-12-09 17:55:47 -0300

    Frontend: aplicando estados aos inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
 import React, { useState } from "react";
 
 const App = () => {
+  const [games, setGames] = useState([]);
   const [title, setTitle] = useState("");
   const [cover, setCover] = useState("");
 
+  
+
   const handleSubmit = (ev) => {
     ev.preventDefault()
-    setTitle(ev.target.value);
-
-    setCover(ev.target.value);
+    console.log({title, cover});
+    setTitle("");
+    setCover("");
   }
 
   return(
     <div id="app">
       <h1>Biblioteca de jogos</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="title">Titulo: </label>
-          <input type="text" name="title" id="title" />
+          <input type="text" name="title" id="title" value={title} onChange={(element) => setTitle(element.target.value)}/>
         </div>
         <div>
           <label htmlFor="cover">Capa: </label>
-          <input type="text" name="cover" id="cover"/>
+          <input type="text" name="cover" id="cover" value={cover} onChange={(element) => setCover(element.target.value)}/>
         </div>
         <button type="submit">Adicionar à Biblioteca</button>
       </form>
